cc7080990f39e66e97a118863f2e62143901f85f
UnterLort <61788072+UnterLort@users.noreply.github.com>
2024-01-26 15:24:16 -0500

    FishTank Java

---
+++ FishTank/FishTank.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @author Kyle Stewart
+ * @version 2
+*/
+
+public class FishTank {
+    /**
+     * 
+     * @param args The passed in commandline arguments
+    */
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("  +=======================+");
+        System.out.println("  |  ><((((*>        _    |");
+        System.out.println("  |                 <_><  |");
+        System.out.println("  |    <)><       <><     |");
+        System.out.println("  |        ><>            |");
+        System.out.println("  |              >('>     |");
+        System.out.println("  +=======================+");
+    }
+}
